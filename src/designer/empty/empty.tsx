eef02b67dc0e36e28729a349d8ce22cd1b496402
heart <7362469@qq.com>
2023-06-15 00:21:29 +0800

    chore: add empty Page

--- src/designer/empty/empty.tsx
+++ src/designer/empty/empty.tsx
@@ -1,7 +1,9 @@
 import {LeftEmpty} from "./leftEmpty"
+import { RightEmpty } from './rightEmpty'
 function EmptyDesigner() {
   return <div className="w-full h-full flex items-center justify-center">
   <LeftEmpty />
+  <RightEmpty />
   </div>
 }
 
